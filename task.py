2b9ade4dcabbc3f5ceb121f99b2d5a194746b078
Laura Santos <68657123+SaintLau@users.noreply.github.com>
2026-03-12 12:25:59 +0000

    Add files via upload

---
+++ task.py
@@ -0,0 +1,6 @@
+greeting = "Hello\n"
+question1 = "Hello! Welcome o your band name generator\nIn what city did you grew up in?\n"
+question2 = "If you had a pet as a child, what was the name?\n"
+
+#print(greeting)
+print("Your band name is " + (input(question1) + input(question2)))
