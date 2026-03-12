486d19dedda7cd912cada869cdcf9e13ddbc715c
Laura Santos <68657123+SaintLau@users.noreply.github.com>
2026-03-12 12:26:58 +0000

    bandGenerator.py

--- task.py
+++ task.py
@@ -1,6 +1,4 @@
-greeting = "Hello\n"
 question1 = "Hello! Welcome o your band name generator\nIn what city did you grew up in?\n"
 question2 = "If you had a pet as a child, what was the name?\n"
 
-#print(greeting)
 print("Your band name is " + (input(question1) + input(question2)))
